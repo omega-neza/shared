32d947f7ba92d446841c98d388a00d0ddf575853
omega-neza <omeganeza@gmail.com>
2024-02-26 17:30:22 +0200

    Galley

--- shared/lib/main.dart
+++ shared/lib/main.dart
@@ -3,15 +3,15 @@ import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:line_icons/line_icons.dart';
 import 'NavBar.dart';
 import 'login_screen.dart';
-import 'sign_up.dart'; 
-import 'screen.dart'; 
+import 'sign_up.dart';
+import 'screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
@@ -23,15 +23,7 @@ class _MyAppState extends State<MyApp> {
       TextStyle(fontSize: 30, fontWeight: FontWeight.w600);
 
   final List<Widget> _widgetOptions = <Widget>[
-    Container(
-      color: const Color.fromARGB(255, 7, 160, 255),
-      child: const Center(
-        child: Text(
-          "Gallery",
-          style: optionStyle,
-        ),
-      ),
-    ),
+    _buildGalleryScreen(),
     Container(
       color: const Color.fromARGB(255, 135, 140, 210),
       child: const Center(
@@ -41,26 +33,57 @@ class _MyAppState extends State<MyApp> {
         ),
       ),
     ),
-    const Screen(), 
+    const Screen(),
     const SignUpScreen(),
     const LoginScreen(),
   ];
 
-  
+  static Widget _buildGalleryScreen() {
+    return Container(
+      color: Color.fromARGB(255, 234, 239, 241),
+      child: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 8.0,
+          mainAxisSpacing: 8.0,
+        ),
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          String imageName = "${index + 1}.jpg";
+          return Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8.0),
+              image: DecorationImage(
+                image: AssetImage("shared/$imageName"),
+                fit: BoxFit.cover,
+              ),
+            ),
+            // You can customize the content of each grid item here
+            child: Center(
+              // child: Text(
+              //   'Picture ${index + 1}',
+              //   style: TextStyle(fontSize: 18, color: Colors.white),
+              // ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
   final ThemeData _lightTheme = ThemeData(
     primarySwatch: Colors.blue,
     hintColor: Colors.amber,
     brightness: Brightness.light,
   );
 
-  
   final ThemeData _darkTheme = ThemeData(
     primarySwatch: Colors.teal,
     hintColor: Colors.deepOrange,
     brightness: Brightness.dark,
   );
 
-  ThemeData _currentTheme = ThemeData.light(); 
+  ThemeData _currentTheme = ThemeData.light();
 
   void _toggleTheme() {
     setState(() {
@@ -68,10 +91,10 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
-   @override
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false, 
+      debugShowCheckedModeBanner: false,
       theme: _currentTheme,
       home: Scaffold(
         drawer: const NavBar(),
@@ -106,7 +129,8 @@ class _MyAppState extends State<MyApp> {
                 gap: 7,
                 activeColor: Colors.blueAccent,
                 iconSize: 24,
-                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
                 tabBackgroundColor: Colors.grey[100]!,
                 color: Colors.black,
                 tabs: const [
@@ -145,130 +169,3 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
-
-
-
-
-
-// import 'package:flutter/material.dart';
-// import 'package:google_nav_bar/google_nav_bar.dart';
-// import 'package:line_icons/line_icons.dart';
-// import 'NavBar.dart';
-// import 'CalculatorScreen.dart';
-// import 'package:shared/login_page.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatefulWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   State<MyApp> createState() => _MyAppState();
-// }
-
-// class _MyAppState extends State<MyApp> {
-//   int _selectedIndex = 0;
-//   static const TextStyle optionStyle =
-//       TextStyle(fontSize: 30, fontWeight: FontWeight.w600);
-
-//   static final List<Widget> _widgetOption = <Widget>[
-//     Container(
-//       color: Color.fromARGB(255, 6, 141, 85),
-//       child: const Center(
-//         child: Text(
-//           "Gallery",
-//           style: optionStyle,
-//         ),
-//       ),
-//     ),
-//     CalculatorScreen(), // Display CalculatorPage directly
-//   ];
-
-//   final ThemeData _lightTheme = ThemeData(
-//     primarySwatch: Colors.blue,
-//     hintColor: Colors.amber,
-//     brightness: Brightness.light,
-//   );
-
-//   final ThemeData _darkTheme = ThemeData(
-//     primarySwatch: Colors.teal,
-//     hintColor: Colors.deepOrange,
-//     brightness: Brightness.dark,
-//   );
-
-//   ThemeData _currentTheme = ThemeData.light();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       theme: _currentTheme,
-//       home: Scaffold(
-//         drawer: NavBar(),
-//         appBar: AppBar(
-//           title: const Text("Navigation Bar"),
-//           actions: [
-//             IconButton(
-//               icon: Icon(Icons.brightness_6),
-//               onPressed: _toggleTheme,
-//             ),
-//           ],
-//         ),
-//         body: Center(
-//           child: _widgetOption.elementAt(_selectedIndex),
-//         ),
-//         bottomNavigationBar: Container(
-//           decoration: BoxDecoration(
-//             color: Colors.white,
-//             boxShadow: [
-//               BoxShadow(
-//                 blurRadius: 30,
-//                 color: Colors.blueGrey.withOpacity(0.1),
-//               ),
-//             ],
-//           ),
-//           child: SafeArea(
-//             child: Padding(
-//               padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
-//               child: GNav(
-//                 rippleColor: Colors.grey[300]!,
-//                 hoverColor: Colors.grey[300]!,
-//                 gap: 7,
-//                 activeColor: const Color.fromARGB(255, 174, 192, 224),
-//                 iconSize: 24,
-//                 padding:
-//                     const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-//                 tabBackgroundColor: Colors.grey[100]!,
-//                 color: Colors.black,
-//                 tabs: const [
-//                   GButton(
-//                     icon: LineIcons.enviraGallery,
-//                     text: "Gallery",
-//                   ),
-//                   GButton(
-//                     icon: LineIcons.calculator,
-//                     text: "Calculator",
-//                   ),
-//                 ],
-//                 selectedIndex: _selectedIndex,
-//                 onTabChange: (index) {
-//                   setState(() {
-//                     _selectedIndex = index;
-//                   });
-//                 },
-//               ),
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-
-//   void _toggleTheme() {
-//     setState(() {
-//       _currentTheme =
-//           _currentTheme == _lightTheme ? _darkTheme : _lightTheme;
-//     });
-//   }
-// }
